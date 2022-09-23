80e25bce7ecb8ceae43cd4384764829eb7c61589
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2022-09-23 06:51:25 +0300

    start app

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, View} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Ahmed app</Text>
+      <TextInput
+      placeholder='inter'
+        accessibilityLabel="input"
+        accessibilityLabelledBy="formLabel"
+      />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
